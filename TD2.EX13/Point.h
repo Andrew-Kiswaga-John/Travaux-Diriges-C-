af280fa86683f70c36bb60fdf333df2d302a8e68
AndyJ047 <148845734+AndyJ047@users.noreply.github.com>
2023-10-24 21:23:35 +0100

    Add files via upload

---
+++ TD2.EX13/Point.h
@@ -0,0 +1,16 @@
+#ifndef POINT_H
+#define POINT_H
+
+class Point {
+	private:
+	float x;
+	float y;
+	
+	public:
+	Point( float x, float y);
+	void deplace( float dx, float dy );
+	void affiche () const;
+	
+};
+
+#endif
